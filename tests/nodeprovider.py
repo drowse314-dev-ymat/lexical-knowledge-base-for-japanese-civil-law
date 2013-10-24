d5b934880dc2b5305a0481e6840b368321d4914f
ymat@macbookair <drowse314@gmail.com>
2013-10-24 14:54:46 +0900

    Separate test fixtures in tests.nodeprovider.

--- tests/nodeprovider.py
+++ tests/nodeprovider.py
@@ -4,6 +4,7 @@ from attest import (
     Tests, assert_hook,
     contextmanager, raises,
 )
+import rdflib
 from lkbutils import nodeprovider
 from lkbutils.nodeprovider import kakasicall
 
@@ -30,6 +31,49 @@ def empty_rdflib_nodeprovider(romanize=True):
         pass
 
 
+class Fixtures:
+    """Namespace for test fixtures."""
+
+    class simple_names:
+        formalized_map = {
+            u'John': u'john',
+            u'Ken': u'ken',
+            u'Yumi is waiting at the door': u'yumi_is_waiting_at_the_door',
+        }
+        not_added = u'poupou'
+        invalid_names = [
+            u'3way',
+            u'魂',
+            u"I'm fine",
+        ]
+
+    class kana_kanji_names:
+        formalized_map = {
+            u'魂': u'tamashii',
+            u'抵当権': u'teitouken',
+            u'補助開始の審判': u'hojokaishinoshinpan',
+        }
+        not_added = u'hitanposaiken'
+
+    class simple_nodenames:
+        formalized_map = {
+            u'John': u'john',
+            u'魂': u'tamashii',
+        }
+        invalid_name = u'3way'
+        not_added = u'詐害行為取消権'
+
+    class simple_properties:
+        names = [u'part_of', u'hyper']
+
+    class kakasi_conversion:
+        desired_conversion = {
+            u'抵当権': u'teitouken',
+            u'国民の休日': u'kokuminnokyuujitsu',
+            u'公共のベランダ': u'koukyounoberanda',
+        }
+
+
 # Adding names.
 
 @nameprovider_unit.test
@@ -37,43 +81,32 @@ def add_simple_names():
     """NameProvider.add."""
     with empty_nameprovider() as provider:
 
-        ret_john = provider.add(u'John')
-        assert ret_john == u'john'
+        for name in Fixtures.simple_names.formalized_map:
+            ret = provider.add(name)
+            expected_formalized = Fixtures.simple_names.formalized_map[name]
+            assert ret == expected_formalized
 
-        ret_ken = provider.add(u'Ken')
-        assert ret_ken == u'ken'
-
-        ret_yumi = provider.add(u'Yumi is waiting at the door')
-        assert ret_yumi == u'yumi_is_waiting_at_the_door'
-
-        with raises(nodeprovider.NameConflict):
-            provider.add(u'John')
-        with raises(nodeprovider.NameConflict):
-            provider.add(u'ken')
+        for name in Fixtures.simple_names.formalized_map:
+            with raises(nodeprovider.NameConflict):
+                provider.add(name)
 
         namespace = provider.ns
-        assert u'john' in namespace
-        assert namespace.john == u'John'
-        assert u'ken' in namespace
-        assert namespace.ken == u'Ken'
-        assert u'yumi_is_waiting_at_the_door' in namespace
-        assert namespace.yumi_is_waiting_at_the_door == u'Yumi is waiting at the door'
+        for name in Fixtures.simple_names.formalized_map:
+            formalized_name = Fixtures.simple_names.formalized_map[name]
+            assert formalized_name in namespace
+            assert getattr(namespace, formalized_name) == name
+            assert provider.get_ns_identifier(name) == formalized_name
 
-        assert provider.get_ns_identifier(u'John') == u'john'
-        assert provider.get_ns_identifier(u'Yumi is waiting at the door') == u'yumi_is_waiting_at_the_door'
         with raises(nodeprovider.NameNotRegistered):
-            provider.get_ns_identifier(u'poupou')
+            provider.get_ns_identifier(Fixtures.simple_names.not_added)
 
 @nameprovider_unit.test
 def add_invalid_names():
     """NameProvider.add refuses invalid names."""
     with empty_nameprovider() as provider:
-        with raises(nodeprovider.InvalidName):
-            provider.add(u'3way')
-        with raises(nodeprovider.InvalidName):
-            provider.add(u'魂')
-        with raises(nodeprovider.InvalidName):
-            provider.add(u"I'm fine")
+        for invalid_name in Fixtures.simple_names.invalid_names:
+            with raises(nodeprovider.InvalidName):
+                provider.add(invalid_name)
 
 @nameprovider_unit.test
 def handle_kana_name_addition():
@@ -82,38 +115,34 @@ def handle_kana_name_addition():
     """
     with empty_nameprovider(romanize=True) as provider:
 
-        ret_soul =  provider.add(u'魂')
-        assert ret_soul == u'tamashii'
-        ret_teitou =  provider.add(u'抵当権')
-        assert ret_teitou == u'teitouken'
-        ret_hojo =  provider.add(u'補助開始の審判')
-        assert ret_hojo == u'hojokaishinoshinpan'
-        with raises(nodeprovider.NameConflict):
-            provider.add(u'teitouken')
+        for name in Fixtures.kana_kanji_names.formalized_map:
+            ret = provider.add(name)
+            expected_formalized = Fixtures.kana_kanji_names.formalized_map[name]
+            assert ret == expected_formalized
+
+        for name in Fixtures.kana_kanji_names.formalized_map:
+            formalized_name = Fixtures.kana_kanji_names.formalized_map[name]
+            with raises(nodeprovider.NameConflict):
+                provider.add(name)
+            with raises(nodeprovider.NameConflict):
+                provider.add(formalized_name)
 
         namespace = provider.ns
-        assert namespace.tamashii == u'魂'
-        assert namespace.teitouken == u'抵当権'
-        assert namespace.hojokaishinoshinpan == u'補助開始の審判'
+        for name in Fixtures.kana_kanji_names.formalized_map:
+            formalized_name = Fixtures.kana_kanji_names.formalized_map[name]
+            assert getattr(namespace, formalized_name) == name
+            assert provider.get_ns_identifier(name) == formalized_name
 
-        assert provider.get_ns_identifier(u'魂') == u'tamashii'
-        assert provider.get_ns_identifier(u'補助開始の審判') == u'hojokaishinoshinpan'
         with raises(nodeprovider.NameNotRegistered):
-            provider.get_ns_identifier(u'hitanposaiken')
+            provider.get_ns_identifier(Fixtures.kana_kanji_names.not_added)
 
 
 # simple kakasi caller.
 @kakasi_unit.test
 def kakasi_conversino():
     """Roughly check kakasi conversion."""
-    desired_yomi = {
-        u'抵当権': u'teitouken',
-        u'国民の休日': u'kokuminnokyuujitsu',
-        u'公共のベランダ': u'koukyounoberanda',
-    }
-
-    for target in desired_yomi:
-        yomi = desired_yomi[target]
+    for target in Fixtures.kakasi_conversion.desired_conversion:
+        yomi = Fixtures.kakasi_conversion.desired_conversion[target]
         assert kakasicall.romanize(target) == yomi
 
 
@@ -128,68 +157,44 @@ def nodeprovider_on_toplevel():
 def add_nodes():
     """(.*)NodeProvider.add."""
 
-    import rdflib
-
     with empty_rdflib_nodeprovider() as provider:
 
-        ret_john = provider.add(u'John')
-        assert isinstance(ret_john, provider.classes['bnode'])
-        assert provider.ns.john == ret_john
-        assert u'john' in provider.ns
-        assert (
-            (provider.ns.john, rdflib.RDFS.label, rdflib.Literal(u'John'))
-            in list(provider.graph.triples((None, None, None)))
-        )
-        assert provider.get(u'John') == provider.ns.john
+        for name in Fixtures.simple_nodenames.formalized_map:
+            ret = provider.add(name)
+            formalized_name = Fixtures.simple_nodenames.formalized_map[name]
+            assert isinstance(ret, provider.classes['bnode'])
+            assert getattr(provider.ns, formalized_name) == ret
+            assert formalized_name in provider.ns
+            assert (
+                (ret, rdflib.RDFS.label, rdflib.Literal(name))
+                in list(provider.graph.triples((None, None, None)))
+            )
+            assert provider.get(name) == ret
 
         with raises(nodeprovider.InvalidName):
-            provider.add(u'3way')
-
-        ret_soul =  provider.add(u'魂')
-        assert isinstance(ret_soul, provider.classes['bnode'])
-        assert provider.ns.tamashii == ret_soul
-        assert u'tamashii' in provider.ns
-        assert (
-            (provider.ns.tamashii, rdflib.RDFS.label, rdflib.Literal(u'魂'))
-            in list(provider.graph.triples((None, None, None)))
-        )
-        assert provider.get(u'魂') == provider.ns.tamashii
+            provider.add(Fixtures.simple_nodenames.invalid_name)
 
         with raises(nodeprovider.NameNotRegistered):
-            provider.get(u'詐害行為取消権')
+            provider.get(Fixtures.simple_nodenames.invalid_name)
+
 
 @nodeprovider_unit.test
 def add_nodes_as_properties():
     """(.*)NodeProvider.add(..., as_property=True)."""
 
-    import rdflib
-
     with empty_rdflib_nodeprovider() as provider:
 
-        ret_part_of = provider.add(u'part_of', as_property=True)
-        assert isinstance(ret_part_of, provider.classes['bnode'])
-        assert provider.ns.part_of == ret_part_of
-        assert u'part_of' in provider.ns
-        assert (
-            (provider.ns.part_of, rdflib.RDFS.label, rdflib.Literal(u'part_of'))
-            in list(provider.graph.triples((None, None, None)))
-        )
-        assert (
-            (provider.ns.part_of, rdflib.RDF.type, rdflib.RDF.Property)
-            in list(provider.graph.triples((None, None, None)))
-        )
-        assert provider.get(u'part_of') == provider.ns.part_of
-
-        ret_hyper = provider.add(u'hyper', as_property=True)
-        assert isinstance(ret_hyper, provider.classes['bnode'])
-        assert provider.ns.hyper == ret_hyper
-        assert u'hyper' in provider.ns
-        assert (
-            (provider.ns.hyper, rdflib.RDFS.label, rdflib.Literal(u'hyper'))
-            in list(provider.graph.triples((None, None, None)))
-        )
-        assert (
-            (provider.ns.hyper, rdflib.RDF.type, rdflib.RDF.Property)
-            in list(provider.graph.triples((None, None, None)))
-        )
-        assert provider.get(u'hyper') == provider.ns.hyper
+        for propname in Fixtures.simple_properties.names:
+            ret = provider.add(propname, as_property=True)
+            assert isinstance(ret, provider.classes['bnode'])
+            assert getattr(provider.ns, propname) == ret
+            assert propname in provider.ns
+            assert (
+                (ret, rdflib.RDFS.label, rdflib.Literal(propname))
+                in list(provider.graph.triples((None, None, None)))
+            )
+            assert (
+                (ret, rdflib.RDF.type, rdflib.RDF.Property)
+                in list(provider.graph.triples((None, None, None)))
+            )
+            assert provider.get(propname) == ret
